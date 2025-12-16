9c7cd4047bcc9a6e14df147ff298ecd5e480819b
Pon4ikx <p.boeva2510@gmail.com>
2025-12-16 23:44:35 +0300

    Настройка БД

--- bernansauto/backend/bernansauto/settings.py
+++ bernansauto/backend/bernansauto/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'bernansauto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BernansAuto_DATABASE',  # Название базы
+        'USER': 'postgres',  # Имя пользователя
+        'PASSWORD': 'postgres',  # Пароль пользователя
+        'HOST': '26.134.120.32',
+        'PORT': '5432',
     }
 }
 
